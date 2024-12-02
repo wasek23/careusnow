7f2ce923b4d748d4bff1e34354e54739ff3b5134
Md. Wasek Bellah <wasekbellah2311@gmail.com>
2024-12-02 22:41:14 +0600

    setup init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+	return <div>
+		<h1 className='text-white text-3xl'>Home</h1>
+
+
+		<Button>Button</Button>
+	</div>
+}
